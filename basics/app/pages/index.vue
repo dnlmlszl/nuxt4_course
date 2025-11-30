23d710fd2f465204f4a5ea9cc6c2c93529d231a9
dnlmlszl <daniel.mlaszlo@yahoo.com>
2025-11-30 16:35:21 +0100

    Composables

--- basics/app/pages/index.vue
+++ basics/app/pages/index.vue
@@ -1,3 +1,17 @@
 <template>
   <h1>Home page</h1>
+
+  <ul v-if="!users.loader.value">
+    <li v-for="user in users.names.value" :key="user">
+      {{ user }}
+    </li>
+  </ul>
+
+  <div v-else>Loading...</div>
+
+  <button @click="users.addUser">Add user name</button>
 </template>
+
+<script setup>
+const users = getUsers('Miles');
+</script>
